b05773793ceaf42f88fc9cc9c2bad6a26c27e440
EnricRG <u1939673@campus.udg.edu>
2019-06-18 11:56:27 +0200

    First test to run minizinc from Scala.

---
+++ src/main/scala/MinizincRun.scala
@@ -0,0 +1,10 @@
+import scala.sys.process._
+
+object MinizincRun extends App{
+  override def main(args: Array[String]): Unit = {
+    var minizinc = Process("bin/minizinc/minizinc.exe --solver Chuffed minizinc/firstModel.mzn minizinc/1er_s1.dzn")
+    val output = (minizinc lineStream) toList
+
+    output foreach(println)
+  }
+}
