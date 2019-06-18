fb300b4f44efacfa6c5e56cc66ecc8042b79a5b4
EnricRG <u1939673@campus.udg.edu>
2019-06-18 11:57:22 +0200

    Changed var to val

--- src/main/scala/MinizincRun.scala
+++ src/main/scala/MinizincRun.scala
@@ -2,7 +2,7 @@ import scala.sys.process._
 
 object MinizincRun extends App{
   override def main(args: Array[String]): Unit = {
-    var minizinc = Process("bin/minizinc/minizinc.exe --solver Chuffed minizinc/firstModel.mzn minizinc/1er_s1.dzn")
+    val minizinc = Process("bin/minizinc/minizinc.exe --solver Chuffed minizinc/firstModel.mzn minizinc/1er_s1.dzn")
     val output = (minizinc lineStream) toList
 
     output foreach(println)
