73f2143a24ffe55ea7ac0f4166a695ef87e889c1
EnricRG <u1939673@campus.udg.edu>
2019-08-04 19:04:03 +0200

    Improved base database implementation. Implemented CourseDatabase.

--- src/main/scala/service/CourseDatabase.scala
+++ src/main/scala/service/CourseDatabase.scala
@@ -1,9 +1,42 @@
 package service
 
+import java.util
+
+import misc.Quarters.Quarter
+
+import scala.collection.JavaConverters._
+import model.{Course, CourseResource}
+
+import scala.collection.mutable.ListBuffer
+import scala.util.control.Exception.Described
+
 class CourseDatabaseInitializer{
 
 }
 
-class CourseDatabase() {
+class CourseDatabase extends Database[String,Course]{
     def this(eventDBInitializer: CourseDatabaseInitializer) = this
+
+    def addCourse(c: Course): Course = {
+        println(c.toString)
+        addElement(c.name, c)
+    }
+
+    def addCourseBooleanResponse(c: Course): Boolean = c == addCourse(c)
+
+
+    def getCourse(course_name: String): Option[Course] = getElement(course_name)
+
+
+    def getCourseOrElse(course_name: String, c: => Course): Course = getElementOrElse(course_name,c)
+
+
+    def removeCourse(course_name: String): Option[Course] = elements.remove(course_name)
+
+
+    def createCourse(name: String, description: Option[String] = None, quarter: Quarter, resources: Traversable[CourseResource] = ListBuffer()): Course =
+            addCourse(new Course(name, description, quarter, resources))
+
+    def createCourse(name: String, description: String, quarter: Quarter, resources: Traversable[CourseResource]): Course =
+            createCourse(name, Some(description), quarter, resources)
 }
--- src/main/scala/service/Database.scala
+++ src/main/scala/service/Database.scala
@@ -6,14 +6,13 @@ abstract class Database[K,E] {
 
     final val elements: mutable.HashMap[K,E] = mutable.HashMap()
 
-    def addElement(key: K, element: E): Option[E] ={
-        elements.contains(key) match {
-            case false => elements.put(key,element)
-            case _ => None
-        }
+    def addElement(key: K, element: E): E ={
+        elements.getOrElseUpdate(key, element)
     }
     def getElement(key: K): Option[E] = elements.get(key)
     def getElementOrElse(key: K, el: => E): E = elements.getOrElse(key,el)
     def removeElement(key: K): Option[E] = elements.remove(key)
+
+    def size: Int = elements.size
 
 }
