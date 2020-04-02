30c67461b0ea97b0084b48506e30126519758aa3
Enric <u1939673@campus.udg.edu>
2020-04-02 20:34:55 +0200

    Modified skeleton for ImportJob modification window.

--- src/main/scala/control/imprt/ImportJobEditorController.scala
+++ src/main/scala/control/imprt/ImportJobEditorController.scala
@@ -4,13 +4,58 @@ import java.net.URL
 import java.util.ResourceBundle
 
 import control.StageController
-import file.imprt.ImportJob
+import file.imprt.{ImportError, ImportJob, ImportType}
+import javafx.fxml.Initializable
+import model.blueprint.{CourseBlueprint, EventBlueprint, ResourceBlueprint, SubjectBlueprint}
+
+import scala.collection.mutable.ArrayBuffer
+
+case class MutableImportJob(
+  var subjects: ArrayBuffer[SubjectBlueprint] = new ArrayBuffer,
+  var events: ArrayBuffer[EventBlueprint] = new ArrayBuffer,
+  var resources: ArrayBuffer[ResourceBlueprint] = new ArrayBuffer,
+  var courses: ArrayBuffer[CourseBlueprint] = new ArrayBuffer,
+  var errors: ArrayBuffer[ImportError] = new ArrayBuffer,
+  var finished: Boolean,
+  var importType: ImportType){
+
+  def toImportJob: ImportJob =
+    ImportJob(
+      subjects.toList,
+      events.toList,
+      resources.toList,
+      courses.toList,
+      errors.toList,
+      finished,
+      importType)
+}
 
 class ImportJobEditorController(importJob: ImportJob) extends StageController {
 
-    override def initialize(location: URL, resources: ResourceBundle): Unit = {
+
+
+  protected val editableImportJob: MutableImportJob = MutableImportJob(
+    new ArrayBuffer ++= importJob.subjects,
+    new ArrayBuffer ++= importJob.events,
+    new ArrayBuffer ++= importJob.resources,
+    new ArrayBuffer ++= importJob.courses,
+    new ArrayBuffer ++= importJob.errors,
+    importJob.finished,
+    importJob.importType)
+
+  override def initialize(location: URL, resources: ResourceBundle): Unit = {
+
+  }
+
+  def getImportJob: ImportJob = editableImportJob.toImportJob
+}
+
+object ImportJobEditorController{
+
+  class SubjectEditorController extends Initializable{
+
+    def initialize(location: URL, resources: ResourceBundle): Unit = {
 
     }
-
-    def getImportJob: ImportJob = throw new UnsupportedOperationException
+  }
 }
--- src/main/scala/file/imprt/ImportJob.scala
+++ src/main/scala/file/imprt/ImportJob.scala
@@ -2,10 +2,12 @@ package file.imprt
 
 import model.blueprint.{CourseBlueprint, EventBlueprint, ResourceBlueprint, SubjectBlueprint}
 
-case class ImportJob(subjects: List[SubjectBlueprint],
-                     events: List[EventBlueprint],
-                     resources: List[ResourceBlueprint],
-                     courses: List[CourseBlueprint],
-                     errors: List[ImportError],
-                     finished: Boolean,
-                     importType: ImportType)
+case class ImportJob(
+    subjects: List[SubjectBlueprint],
+    events: List[EventBlueprint],
+    resources: List[ResourceBlueprint],
+    courses: List[CourseBlueprint],
+    errors: List[ImportError],
+    finished: Boolean,
+    importType: ImportType
+)
